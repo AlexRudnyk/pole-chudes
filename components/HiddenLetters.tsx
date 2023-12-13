22f6148e28e23b8ad4ba000c193442a6ece3e72a
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-12-13 13:16:15 +0200

    Update HiddenLetters.tsx

--- components/HiddenLetters.tsx
+++ components/HiddenLetters.tsx
@@ -5,15 +5,24 @@ import React, { useEffect, useState } from "react";
 import { Gamer, Letter, LetterInput } from ".";
 import Image from "next/image";
 import Yakubovich from "../public/yakubovich.jpg";
-import { getNextUniqueIndex } from "@/utils";
+import { getNextUniqueIndex, getRandomIntInclusive } from "@/utils";
 
 const HiddenLetters = () => {
   const [letter, setLetter] = useState<string>("");
   const [guessed, setGuessed] = useState<string[]>([]);
   const [secondGamer, setsecondGamer] = useState<boolean>(false);
   const [index, setIndex] = useState<number>(0);
+  const [uniqueIndex, setUniqueIndex] = useState<number>(0);
 
-  const answer = tasks[index].answer.split("");
+  useEffect(() => {
+    setIndex(getRandomIntInclusive());
+  }, []);
+
+  useEffect(() => {
+    setUniqueIndex(getNextUniqueIndex(index));
+  }, []);
+
+  const answer = tasks[uniqueIndex].answer.split("");
   const guessedLetter = answer.find((item) => item === letter);
 
   const uniqueLettersInAnswer = [...new Set(answer)];
@@ -23,7 +32,7 @@ const HiddenLetters = () => {
   const handleNextQuestion = () => {
     setLetter("");
     setGuessed([]);
-    setIndex(() => getNextUniqueIndex(index));
+    setIndex(() => getNextUniqueIndex(uniqueIndex));
   };
 
   const toggleGamer = () => {
@@ -48,7 +57,7 @@ const HiddenLetters = () => {
 
   return (
     <>
-      <h1 className="text-3xl mb-12">Вопрос: {tasks[index].question}</h1>
+      <h1 className="text-3xl mb-12">Вопрос: {tasks[uniqueIndex].question}</h1>
       <LetterInput onSubmit={handleletterSubmit} />
       <ul className="flex justify-center items-center mb-16">
         {answer.map((letter: string, index) => (
